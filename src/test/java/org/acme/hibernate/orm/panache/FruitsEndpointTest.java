4538458ad4dd4ec1579b8778e534a0e97774740b
Rafael T. C. Soares <rafaelcba@gmail.com>
2022-07-29 15:16:32 -0400

    adding support for h2 and openapi

--- src/test/java/org/acme/hibernate/orm/panache/FruitsEndpointTest.java
+++ src/test/java/org/acme/hibernate/orm/panache/FruitsEndpointTest.java
@@ -6,9 +6,12 @@ import static org.hamcrest.core.IsNot.not;
 
 import org.junit.jupiter.api.Test;
 
+import io.quarkus.test.common.QuarkusTestResource;
+import io.quarkus.test.h2.H2DatabaseTestResource;
 import io.quarkus.test.junit.QuarkusTest;
 
 @QuarkusTest
+@QuarkusTestResource(H2DatabaseTestResource.class)
 public class FruitsEndpointTest {
 
     @Test
