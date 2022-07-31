275f77eadea5609a4b2f396facb02048a982d342
Rafael Soares <rafaelcba@gmail.com>
2022-07-31 23:13:13 +0000

    ajusting quarkus profiles

---
+++ src/main/java/org/acme/lifecycle/AppLifecycleBean.java
@@ -0,0 +1,25 @@
+package org.acme.lifecycle;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.event.Observes;
+
+import io.quarkus.runtime.ShutdownEvent;
+import io.quarkus.runtime.StartupEvent;
+import org.jboss.logging.Logger;
+
+import io.quarkus.runtime.configuration.ProfileManager;
+
+@ApplicationScoped
+public class AppLifecycleBean {
+
+    private static final Logger LOGGER = Logger.getLogger("ListenerBean");
+
+    void onStart(@Observes StartupEvent ev) {               
+        LOGGER.info("The application is starting with profile " + ProfileManager.getActiveProfile());
+    }
+
+    void onStop(@Observes ShutdownEvent ev) {               
+        LOGGER.info("The application is stopping...");
+    }
+
+}
